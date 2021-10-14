a4e276caf93a74aa99b89e71ae2aecb1f341acaf
Murat Kilic <muratkilic1978@gmail.com>
2021-10-14 07:49:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+    const monkey = document.getElementById("monkeyAudio"); 
+    const horn = document.getElementById("hornAudio"); 
+    
+    function playMonkeyAudio() { 
+      monkey.play(); 
+    } 
+    
+    function pauseMonkeyAudio() { 
+      monkey.pause(); 
+    } 
+    
+    function playHornAudio() { 
+      horn.play(); 
+    } 
+    
+    function pauseHornAudio() { 
+      horn.pause(); 
+    } 
